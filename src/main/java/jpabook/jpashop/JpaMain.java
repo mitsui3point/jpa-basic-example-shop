33433b0b855e996c848c2ab67e98a689f54b23cf
mitsui3point <rladbsgh0331@gmail.com>
2022-12-01 12:36:50 +0900

    feat($jpaMain, $domain): update - update 글로벌 페치 전략 설정 • 모든 연관관계를 지연 로딩으로 • @ManyToOne, @OneToOne은 기본이 즉시 로딩이므로 지연 로딩으로 변경
    - update 영속성 전이 설정
    • Order -> Delivery를 영속성 전이 ALL 설정
    • Order -> OrderItem을 영속성 전이 ALL 설정

--- src/main/java/jpabook/jpashop/JpaMain.java
+++ src/main/java/jpabook/jpashop/JpaMain.java
@@ -1,7 +1,11 @@
 package jpabook.jpashop;
 
+import jpabook.jpashop.constant.DeliveryStatus;
+import jpabook.jpashop.constant.OrderStatus;
+import jpabook.jpashop.domain.Delivery;
+import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.domain.OrderItem;
 import jpabook.jpashop.domain.item.Album;
-import jpabook.jpashop.domain.item.Item;
 
 import java.time.LocalDateTime;
 
@@ -10,17 +14,32 @@ import static jpabook.jpashop.context.JpaPersistenceContext.create;
 public class JpaMain {
     public static void main(String[] args) {
         create(em -> {
-            Item album = Album.create("name", 1000, 10, "artist1", "etc1");
-            album.setCreatedDate(LocalDateTime.now());
+            Album album = Album.create("album1", 10000, 100, "artist1", "etc1");
 
+            OrderItem orderItem = OrderItem.create(album, album.getPrice(), album.getStockQuantity());
+
+            Delivery delivery = Delivery.create("city", "street", "zipcode", DeliveryStatus.DELIVERY);
+
+            Order order = Order.create(LocalDateTime.now(), OrderStatus.ORDER);
+            order.addOrderItems(orderItem);
+            order.putDelivery(delivery);
+
+            em.persist(order);
+//            em.persist(orderItem);
+//            em.persist(delivery);
             em.persist(album);
 
             em.flush();
             em.clear();
 
-            Item findAlbum = em.find(Item.class, album.getId());
-            System.out.println("findAlbum = " + findAlbum.getCreatedDate());
+            System.out.println("====================1");
+            OrderItem findOrderItem = em.find(OrderItem.class, orderItem.getId());
+            System.out.println("====================2");
+            OrderStatus status = findOrderItem.getOrder().getStatus();
+            System.out.println("====================3");
 
+            em.flush();
+            em.clear();
         });
     }
 }
--- src/main/java/jpabook/jpashop/domain/Delivery.java
+++ src/main/java/jpabook/jpashop/domain/Delivery.java
@@ -13,7 +13,7 @@ public class Delivery extends BaseEntity {
     @Column(name = "DELIVERY_ID")
     private Long id;
 
-    @OneToOne(mappedBy = "delivery")
+    @OneToOne(mappedBy = "delivery", fetch = FetchType.LAZY)
     private Order order;
 
     private String city;
@@ -25,48 +25,34 @@ public class Delivery extends BaseEntity {
     public Long getId() {
         return id;
     }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
     public Order getOrder() {
         return order;
     }
-
-    public void setOrder(Order order) {
-        this.order = order;
-    }
-
     public String getCity() {
         return city;
     }
-
-    public void setCity(String city) {
-        this.city = city;
-    }
-
     public String getStreet() {
         return street;
     }
-
-    public void setStreet(String street) {
-        this.street = street;
-    }
-
     public String getZipcode() {
         return zipcode;
     }
-
-    public void setZipcode(String zipcode) {
-        this.zipcode = zipcode;
-    }
-
     public DeliveryStatus getStatus() {
         return status;
     }
 
-    public void setStatus(DeliveryStatus status) {
-        this.status = status;
+    //== 연관관계 편의 메서드==//
+
+    public static Delivery create(String city, String street, String zipcode, DeliveryStatus status) {
+        Delivery delivery = new Delivery();
+        delivery.city = city;
+        delivery.street = street;
+        delivery.zipcode = zipcode;
+        delivery.status = status;
+        return delivery;
+    }
+
+    public void putOrder(Order order) {
+        this.order = order;
     }
 }
